8f8eb042b65cdc64559c8c31f696e453bb1f2a71
fatinys <fatinsyasin@gmail.com>
2024-08-28 12:56:11 -0500

    app

--- windturbineoptimization/app.py
+++ windturbineoptimization/app.py
@@ -7,25 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        # Get values from the form
-        year_built = int(request.form['year_built'])
-        hub_height = float(request.form['hub_height'])
-        rotor_diameter = float(request.form['rotor_diameter'])
-        location = request.form['location']
-
-        # Create a DataFrame with the input
-        input_data = pd.DataFrame({
-            'Year Built': [year_built],
-            'Hub Height': [hub_height],
-            'Rotor Diameter': [rotor_diameter],
-            'Location': [location]
-        })
-
-    
-        return render_template('index.html', prediction=prediction)
-
-    return render_template('index.html')
+    return "Hallo"
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
